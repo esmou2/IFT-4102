d07df61c29f6c09b00f6330d95138a8f5196f36f
Esma <esma.mouine.1@ulaval.ca>
2018-03-23 20:15:53 -0400

    bayes focntionnel

--- Code/test.py
+++ Code/test.py
@@ -12,11 +12,10 @@ def occurrences(labels):
 		return prob
 
 
-train, train_labels, test, test_labels = load_datasets.load_iris_dataset(0.1)
+train, train_labels, test, test_labels = load_datasets.load_congressional_dataset(1)
 
 knn = Knn.Knn(5)
-conversion_labels = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
-
+conversion_labels = ['republican','democrat']
 
 train = np.array(train)
 
@@ -27,7 +26,6 @@ for cls in classes:
     likelihoods[cls] = defaultdict(list)
 
 class_probabilities = occurrences(train_labels)
-
 for cls in classes:
     row_indices = []
     for i in range(len(train_labels)):
@@ -39,10 +37,13 @@ for cls in classes:
         likelihoods[cls][j] += list(subset[:,j])
 
 for cls in classes:
-    for j in range(0, c):
+    for j in range(cols):
         likelihoods[cls][j] = occurrences(likelihoods[cls][j])
-new_sample = [5.1,3.5,1.4,0.2]
+
+new_sample = [1,1,1,0,0,0,1,1,1,0,1,0,0,0,1,1]
+
 results = {}
+
 for cls in classes:
     class_probability = class_probabilities[cls]
     for i in range(len(new_sample)):
@@ -53,4 +54,4 @@ for cls in classes:
             class_probability *= 0
         results[cls] = class_probability
 
-print results
+print conversion_labels[max(results, key=lambda i: results[i])]
